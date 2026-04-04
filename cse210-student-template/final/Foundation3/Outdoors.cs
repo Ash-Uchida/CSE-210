c98fd259bdbbf5828c1adeb9af7b678e8e9d1c8c
Ash Uchida <ash.uchida0w0.gmail.com>
2026-04-04 10:34:23 -0600

    extra

--- cse210-student-template/final/Foundation3/Outdoors.cs
+++ cse210-student-template/final/Foundation3/Outdoors.cs
@@ -1,15 +1,26 @@
 class Outdoors : Event
 {
-    protected string _weather;
-    public Outdoors(string title, string desc, string date, string time, Address address, string weather) 
+    protected List<string> _weather = new List<string>{"Sunny", "Partly Cloudy", "Cloudy", "Overcast","Raining", "Drizzle", "Thunderstorm","Snowing", "Blizzard", "Sleet", "Hail", "Windy", "Breezy", "Foggy", "Misty", "Hot", "Cold", "Freezing", "Humid", "Dry", "Clear", "Stormy"};
+    public Outdoors(string title, string desc, string date, string time, Address address) 
     : base(title, desc, date, time, address)
     {
-        _weather = weather;
+        //we dont need this cause the _weather varible is only used in this class and not something we retriving from somewhere
     } 
+
+    public string RandWeather()
+    {
+        // we choose a random index from the list
+        Random random = new();
+        int weatherIndex = random.Next(0,_weather.Count);
+        return _weather[weatherIndex];
+    }
     
     public override string FullDetails() 
     {
         // For receptions this includes an email for RSVP.
-        return $"{StandardDetails()}\nType: Outdoors\nWeather: {_weather}";
+        return $"{StandardDetails()}\nType: Outdoors\nWeather: {RandWeather()}";
     }
 }
+
+// for extra credit add randomized weather because wether is random, this predicts what the weather is 
+// and allows you to change your descion depending on the chaning weather predictions
--- cse210-student-template/final/Foundation3/Program.cs
+++ cse210-student-template/final/Foundation3/Program.cs
@@ -17,7 +17,7 @@ class Program
         //lets create an event for each class
         Lectures lec1 = new Lectures("Community Leadership Talk", "A discussion on leadership principles and how to create a positive impact within your community.","April 15, 2026", "10:30AM", add1, "Alex Anderson", 150);
         Receptions rep1 = new Receptions("Midnight Masquerade Mixer", "An elegant evening of masks, music, and mystery. Dress to impress and enjoy light refreshments.", "October 31, 2026", "8:00 PM", add2,true,"masquerade@midnightmail.com");
-        Outdoors out1 = new Outdoors("Mountain Morning Meetup", "Start your day with a refreshing hike and scenic views. Great for all skill levels!", "June 10, 2026", "7:30 AM", add3,"Sunny with a cool breeze");
+        Outdoors out1 = new Outdoors("Mountain Morning Meetup", "Start your day with a refreshing hike and scenic views. Great for all skill levels!", "June 10, 2026", "7:30 AM", add3);
 
         // adding the events to the events list then we loop through it and display it
         _events.Add(lec1);
